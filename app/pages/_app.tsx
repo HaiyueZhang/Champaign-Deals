6da3c186485af09d2c6b80afcf208b2b5e1e2cf6
RaymondWHZ <wuhaozhen1106@gmail.com>
2022-12-07 15:25:57 -0600

    Add website title

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,10 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {ChakraProvider} from "@chakra-ui/react";
 import Layout from './layout';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Champaign Deals</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
